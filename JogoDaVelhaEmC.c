bafba4b36b05c6dc51346abe409a94991bf5325c
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2023-12-03 09:16:42 -0300

    Comentário função preenchematrizint

--- JogoDaVelhaEmC.c
+++ JogoDaVelhaEmC.c
@@ -225,7 +225,7 @@ void preenchematrizchar (char matriz[3][3]) {
     }
 }
 
-void preenchematrizint (int matriz[][3]) {
+void preenchematrizint (int matriz[][3]) { // Apenas preenche as células da matriz com números de 1 a 9, a fim de que o usuário possa localizar a posição que deseja jogar.
     int a = 1;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
